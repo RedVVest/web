4494533e98bf21ace23c6f955bfedf0aba147f25
RedVVest <109755387+RedVVest@users.noreply.github.com>
2022-08-05 14:51:18 +0300

    1

---
+++ js/main.js
@@ -0,0 +1,40 @@
+
+
+var owl = $('.owl-carousel');
+owl.owlCarousel(
+  {
+    center: true,
+    loop: true,
+    margin: 20,
+    startPosition: 1,
+    items: 1,
+    responsive: {
+    540: {
+      items: 3,
+      startPosition: 1,
+    },
+    1200: {
+      items: 3,
+      margin: 30,
+    },
+  },
+  });
+
+// Go to the previous item
+$('.btn__arrow').click(function() {
+    // With optional speed parameter
+    // Parameters has to be in square bracket '[]'
+    owl.trigger('prev.owl.carousel');
+})
+
+
+const navBtn = document.querySelector('.nav__toggle');
+const nav = document.querySelector('.nav')
+const menuIcon = document.querySelector('.menu-icon');
+
+
+navBtn.onclick = function () {
+  nav.classList.toggle('nav--mobile')
+  menuIcon.classList.toggle('menu-icon--active');
+  document.body.classList.toggle('no-scroll');
+};
